84a4f404e711f8d52793c2f38c434935860bdf5e
Kenny Rigaud <rigaudkenny@gmail.com>
2023-08-26 17:32:57 -0400

    animation general dans projet html

--- loader.js
+++ loader.js
@@ -35,10 +35,49 @@ Pace.on('done', () =>{
        // start other animation here
        if ( document.URL.includes("projet.html")){
            let tl = gsap.timeline()
+
+           let menu = document.getElementById("menu")
+           menu.style.opacity = 0
+
+           let showText1 = document.querySelectorAll(".show-text-1")
+           let showText2 = document.querySelectorAll(".show-text-2")
+           
+           showText1.forEach(element => {
+            element.style.opacity = 0
+           });
+           showText2.forEach(element => {
+            element.style.opacity = 0
+           });
+           
+
+          document.querySelector(".logo").style.opacity = 0
+          tl.add("go")
            tl.from("h1",{
-            duration :3,
+            duration :2,
             x: 150
+           },"go")
+           tl.to(".logo",{
+            duration:1,
+            opacity:1
            })
+           tl.to("#menu",{
+            duration:1,
+            opacity:1
+           })
+           tl.to(showText1,{
+            duration:0.5,
+            opacity:1
+           })
+           tl.to(showText2,{
+            duration:0.5,
+            opacity:1
+           })
+           tl.from(".li-categorie",{
+            duration:1,
+            opacity: 0,
+            y: -150,
+            stagger: 0.5 
+           },"go")
        }
     
 
